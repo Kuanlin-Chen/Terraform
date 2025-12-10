3eec02e7e07bdcfd8d1afc0c508ee48bddb039b3
Kuanlin-Chen <webb1992810625@gmail.com>
2025-12-10 08:59:18 +0100

    Add password length validation

--- sensitive_data_test/main.tf
+++ sensitive_data_test/main.tf
@@ -6,6 +6,11 @@ variable "password" {
   description = "Demo secret"
   type        = string
   sensitive   = true
+
+  validation {
+    condition = length(var.password) >= 8
+    error_message = "password must be longer than 8 characters"
+  }
 }
 
 resource "local_file" "demo" {
