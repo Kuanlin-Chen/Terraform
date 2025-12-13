9e03549d993d64854f9845641d475eb9f0ba8c86
Kuanlin-Chen <webb1992810625@gmail.com>
2025-12-13 21:32:55 +0100

    Add compute module in lbs_app

---
+++ lbs_app/modules/compute/variables.tf
@@ -0,0 +1,29 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-09a38e2e7a3cc42de" # Ubuntu Server 24.04 LTS
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address"
+  type        = bool
+  default     = false
+}
+
+variable "user_data" {
+  description = "User data script for the EC2 instance"
+  type        = string
+  default     = ""
+}
